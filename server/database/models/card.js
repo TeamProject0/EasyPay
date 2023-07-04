31bb4027abbee448f8f96ba987eb7c35abd97a50
Salami <medselimthabet2@gmail.com>
2023-07-04 12:39:49 +0100

    back-end fixed

--- server/database/models/card.js
+++ server/database/models/card.js
@@ -3,26 +3,26 @@ const conn = require("../index.js")
 module.exports = {
     getAll: function (callback) {
       const q = 'SELECT * FROM card'
-      conn.query(q, function (error, results, fields) {
+      conn.query(q, function (error, results) {
         callback(error, results);
       });
     },
      
-      add: function(callback, values) {
-        const q = "INSERT INTO card (cardnumber,cvv,balance,expiry,brand) VALUES (?,?,?,?,?)";
-        conn.query(q, [values.cardnumber,values.cvv,values.balance,values.expiry,values.brand], function(err, results, fields) {
+      add: function(callback, values,iduser) {
+        const q = "INSERT INTO card (cardnumber,cvv,balance,expiry,brand,users_idusers) VALUES (?,?,?,?,?,?)";
+        conn.query(q, [values.cardnumber,values.cvv,values.balance,values.expiry,values.brand,iduser], function(err, results) {
           callback(err, results);
         });
       },
       delete: function(idcards,callback ) {
-        const q = "DELETE FROM card WHERE idcard = ?";
-        conn.query(q,idcards, function(err, results, fields) {
+        const q = `DELETE FROM card WHERE idcard = ${idcards}`
+        conn.query(q, function(err, results) {
           callback(err, results);
         });
       },
-      update: function(idcards,cardnumber,cvv,balance,expiry,brand,callback ) {
-        const q = "UPDATE  card SET cardnumber=?, `cvv`=?, balance=?, expiry=?, brand=? WHERE idcard = ?";
-        conn.query(q,[idcards,cardnumber,cvv,balance,expiry,brand], function(err, results, fields) {
+      update: function(values,idcard,callback ) {
+        const q = `UPDATE card SET balance=${values.balance} WHERE idcard =${idcard}`;
+        conn.query(q, function(err, results) {
           callback(err, results);
         });
       }
