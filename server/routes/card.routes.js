dbba5b35772b28a299870103a255d52f06cda0c7
samarzribi <samarzribi014@gmail.com>
2023-07-04 12:47:07 +0100

    test

--- server/routes/card.routes.js
+++ server/routes/card.routes.js
@@ -1,11 +1,10 @@
 const express = require("express")
 const router = express.Router()
 
-const { getAllCard, addCard,deleteCard,updateCard } = require("../controllers/card");
+const { getAllCard, addCard,updateCard } = require("../controllers/card");
 
 
 router.get("/getAll",getAllCard)
 router.post("/add",addCard)
-router.delete("/:idcards",deleteCard)
 router.put("/:idcards",updateCard)
 module.exports = router;
