b5a691cca7a82ebacc5cc0986f6889293ebf5d4a
aymen saidani <aymensaidany@gmail.com>
2023-07-04 16:29:50 +0200

    cloudinary

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require("cors")
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 const usersRoute = require("./routes/users.routes")
 const cardRoute = require("./routes/card.routes")
