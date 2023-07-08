4fe8ebeb206b86b44978af05bb8df6a84e074c6b
Salami <medselimthabet2@gmail.com>
2023-07-08 02:14:22 +0100

    logged user works

--- client/src/components/CreditCard.jsx
+++ client/src/components/CreditCard.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const CreditCard = () => {
   return (
-    <div className="flip-card">
-    <div className="flip-card-inner">
+    <div className="flip-card al">
+      <div className="flip-card-inner">
         <div className="flip-card-front">
-            <p className="heading_8264">MASTERCARD</p>
-            <svg className="logo" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 48 48">
+          <p className="heading_8264">MASTERCARD</p>
+          <svg className="logo" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="36" height="36" viewBox="0 0 48 48">
             <path fill="#ff9800" d="M32 10A14 14 0 1 0 32 38A14 14 0 1 0 32 10Z"></path><path fill="#d50000" d="M16 10A14 14 0 1 0 16 38A14 14 0 1 0 16 10Z"></path><path fill="#ff3d00" d="M18,24c0,4.755,2.376,8.95,6,11.48c3.624-2.53,6-6.725,6-11.48s-2.376-8.95-6-11.48 C20.376,15.05,18,19.245,18,24z"></path>
-            </svg>
-            <svg version="1.1" className="chip" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 50 50" xmlSpace="preserve">  <image id="image0" width="50" height="50" x="0" y="0" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAMAAAAp4XiDAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+          </svg>
+          <svg version="1.1" className="chip" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="30px" height="30px" viewBox="0 0 50 50" xmlSpace="preserve">  <image id="image0" width="50" height="50" x="0" y="0" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAMAAAAp4XiDAAAABGdBTUEAALGPC/xhBQAAACBjSFJN
               AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAB6VBMVEUAAACNcTiVeUKVeUOY
               fEaafEeUeUSYfEWZfEaykleyklaXe0SWekSZZjOYfEWYe0WXfUWXe0WcgEicfkiXe0SVekSXekSW
               ekKYe0a9nF67m12ZfUWUeEaXfESVekOdgEmVeUWWekSniU+VeUKVeUOrjFKYfEWliE6WeESZe0GS
@@ -35,8 +35,8 @@ const CreditCard = () => {
               cmVhdGUAMjAyMy0wMi0xM1QwODoxNToyOSswMDowMEUnN7UAAAAldEVYdGRhdGU6bW9kaWZ5ADIw
               MjMtMDItMTNUMDg6MTU6MjkrMDA6MDA0eo8JAAAAKHRFWHRkYXRlOnRpbWVzdGFtcAAyMDIzLTAy
               LTEzVDA4OjE1OjI5KzAwOjAwY2+u1gAAAABJRU5ErkJggg=="></image>
-            </svg>
-            <svg version="1.1" className="contactless" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 50 50" xmlSpace="preserve">  <image id="image0" width="50" height="50" x="0" y="0" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAQAAAC0NkA6AAAABGdBTUEAALGPC/xhBQAAACBjSFJN
+          </svg>
+          <svg version="1.1" className="contactless" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="20px" height="20px" viewBox="0 0 50 50" xmlSpace="preserve">  <image id="image0" width="50" height="50" x="0" y="0" href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADIAAAAyCAQAAAC0NkA6AAAABGdBTUEAALGPC/xhBQAAACBjSFJN
               AAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZ
               cwAACxMAAAsTAQCanBgAAAAHdElNRQfnAg0IEzgIwaKTAAADDklEQVRYw+1XS0iUURQ+f5qPyjQf
               lGRFEEFK76koKGxRbWyVVLSOgsCgwjZBJJYuKogSIoOonUK4q3U0WVBWFPZYiIE6kuArG3VGzK/F
@@ -56,21 +56,21 @@ const CreditCard = () => {
               MTNUMDg6MTk6NTYrMDA6MDCjlq7LAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDIzLTAyLTEzVDA4OjE5
               OjU2KzAwOjAw0ssWdwAAACh0RVh0ZGF0ZTp0aW1lc3RhbXAAMjAyMy0wMi0xM1QwODoxOTo1Nisw
               MDowMIXeN6gAAAAASUVORK5CYII="></image>
-            </svg>
-            <p className="number">9759 2484 5269 6576</p>
-            <p className="valid_thru">VALID THRU</p>
-            <p className="date_8264">1 2 / 2 4</p>
-            <p className="name">BRUCE WAYNE</p>
+          </svg>
+          <p className="number">9759 2484 5269 6576</p>
+          <p className="valid_thru">VALID THRU</p>
+          <p className="date_8264">1 2 / 2 4</p>
+          <p className="name">BRUCE WAYNE</p>
         </div>
         <div className="flip-card-back">
-            <div className="strip"></div>
-            <div className="mstrip"></div>
-            <div className="sstrip">
-              <p className="code">***</p>
-            </div>
+          <div className="strip"></div>
+          <div className="mstrip"></div>
+          <div className="sstrip">
+            <p className="code">***</p>
+          </div>
         </div>
+      </div>
     </div>
-</div>
   )
 }
 
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './navbar/Navbar.jsx'
 import Card from './Card.jsx'
 import CreditCard from './CreditCard.jsx'
+import { useLocation } from 'react-router-dom'
 
 const Home = () => {
+  const location = useLocation()
+  const data = location.state
+
+
+  console.log("data in home", data);
   return (
     <div>
-        <Navbar/>
-        <CreditCard/>
-        <Card/>
+      <Navbar data={data} />
+      <div className='al'>
+        <Card />
+        <CreditCard />
+      </div>
     </div>
   )
 }
